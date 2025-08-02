599e05f64b4c14c59c6e7e1d1571b4c72011ac25
jonilgz <hi@jonilgz.com>
2025-08-02 11:53:40 +0200

    refactor settings tabs

--- Source/Mod_SeedsPlease.cs
+++ Source/Mod_SeedsPlease.cs
@@ -2,7 +2,6 @@
 using Verse;
 using UnityEngine;
 using RimWorld;
-using System.Linq;
 using System.Collections.Generic;
 using static SeedsPleaseLite.ModSettings_SeedsPleaseLite;
 
@@ -21,69 +20,67 @@ namespace SeedsPleaseLite
 			//========Setup tabs=========
 			GUI.BeginGroup(inRect);
 
-			Rect rect = new Rect(0f, 32f, inRect.width, inRect.height - 32f);
-			Widgets.DrawMenuSection(rect);
-			
-			Listing_Standard options = new Listing_Standard();
-			options.Begin(inRect.ContractedBy(15f));
-			options.Label("SPL.RequiresRestart".Translate());
-			options.GapLine();
-			options.Label("SPL.Settings.MarketValueModifier".Translate("100%", "20%", "500%") + marketValueModifier.ToStringPercent(), -1f, "SPL.Settings.MarketValueModifier.Desc".Translate());
-			marketValueModifier = options.Slider(marketValueModifier, 0.2f, 5f);
+			// Tabs and main config area below the top
+			float topOffset = 0f;
+			Rect mainRect = new Rect(0f, topOffset, inRect.width, inRect.height - topOffset);
+			Widgets.DrawMenuSection(mainRect);
 
-			options.Label("SPL.Settings.SeedExtractionModifier".Translate("100%", "20%", "500%") + extractionModifier.ToStringPercent(), -1f, "SPL.Settings.SeedExtractionModifier.Desc".Translate("4"));
-			extractionModifier = options.Slider(extractionModifier, 0.2f, 5f);
-
-			options.Label("SPL.Settings.SeedFactorModifier".Translate("100%", "20%", "500%") + seedFactorModifier.ToStringPercent(), -1f, "SPL.Settings.SeedFactorModifier.Desc".Translate("1"));
-			seedFactorModifier = options.Slider(seedFactorModifier, 0.2f, 5f);
-
-			options.CheckboxLabeled("SPL.Settings.NoUselessSeeds".Translate(), ref noUselessSeeds, "SPL.Settings.NoUselessSeeds.Desc".Translate());
-			options.CheckboxLabeled("SPL.Settings.ClearSnow".Translate(), ref clearSnow, "SPL.Settings.ClearSnow.Desc".Translate());
-
-			options.CheckboxLabeled("SPL.Settings.EdibleSeeds".Translate(), ref edibleSeeds, "SPL.Settings.EdibleSeeds.Desc".Translate());
-
-			//============
-
-			//Record positioning before closing out the lister...
-			Rect seedlessFilterRect = inRect.ContractedBy(15f);
-			// curY and listingRect are not accessible; use options.GetCurY() if available, otherwise estimate position
-			float curY = options.GetType().GetProperty("CurY", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.NonPublic)?.GetValue(options) as float? ?? 0f;
-			seedlessFilterRect.y = curY + 95f;
-			seedlessFilterRect.height = inRect.height - curY - 105f; //Use remaining space
-
-			// ColumnWidth: set to inRect width minus margin
-			options.ColumnWidth = inRect.width - 30f;
-			options.End();
-
-			//========Setup tabs=========
+			// Tabs
 			var tabs = new List<TabRecord>();
+			tabs.Add(new TabRecord("Main", delegate { selectedTab = Tab.main; }, selectedTab == Tab.main));
 			tabs.Add(new TabRecord("Seedless", delegate { selectedTab = Tab.seedless; }, selectedTab == Tab.seedless));
-			tabs.Add(new TabRecord("Labels", delegate { selectedTab = Tab.labels; }, selectedTab == Tab.labels));
-			
-			Widgets.DrawMenuSection(seedlessFilterRect); //Used to make the background light grey with white border
-			TabDrawer.DrawTabs(new Rect(seedlessFilterRect.x, seedlessFilterRect.y, seedlessFilterRect.width, Text.LineHeight), tabs);
+			//tabs.Add(new TabRecord("Labels", delegate { selectedTab = Tab.labels; }, selectedTab == Tab.labels));
+			float tabY = topOffset + 40f;
+			TabDrawer.DrawTabs(new Rect(15f, tabY, inRect.width - 30f, Text.LineHeight), tabs);
 
-			//========Between tabs and scroll body=========
-			options.Begin(new Rect (seedlessFilterRect.x + 10, seedlessFilterRect.y + 10, seedlessFilterRect.width - 10f, seedlessFilterRect.height - 10f));
-				if (selectedTab == Tab.seedless)
-				{
-					options.Label("SPL.Settings.SeedlessDesc".Translate());
-				}
-				else
-				{
-					options.Label("SPL.Settings.LabelsDesc".Translate());
-				}
-			options.End();
-			//========Scroll area=========
-			seedlessFilterRect.y += 30f;
-			seedlessFilterRect.yMax -= 30f;
-			Rect weaponsFilterInnerRect = new Rect(0f, 0f, seedlessFilterRect.width - 30f, (OptionsDrawUtility.lineNumber + 2) * 22f);
-			Widgets.BeginScrollView(seedlessFilterRect, ref scrollPos, weaponsFilterInnerRect , true);
-				options.Begin(weaponsFilterInnerRect);
-				options.DrawList(inRect);
+			float contentY = tabY + Text.LineHeight + 8f;
+			float contentHeight = inRect.height - (tabY + Text.LineHeight + 16f);
+			Rect contentRect = new Rect(15f, contentY, inRect.width - 30f, contentHeight);
+
+			if (selectedTab == Tab.main)
+			{
+				Listing_Standard options = new Listing_Standard();
+				options.Begin(contentRect);
+				options.GapLine();
+				options.Label("SPL.Settings.MarketValueModifier".Translate("100%", "20%", "500%") + marketValueModifier.ToStringPercent(), -1f, "SPL.Settings.MarketValueModifier.Desc".Translate());
+				marketValueModifier = options.Slider(marketValueModifier, 0.2f, 5f);
+
+				options.Label("SPL.Settings.SeedExtractionModifier".Translate("100%", "20%", "500%") + extractionModifier.ToStringPercent(), -1f, "SPL.Settings.SeedExtractionModifier.Desc".Translate("4"));
+				extractionModifier = options.Slider(extractionModifier, 0.2f, 5f);
+
+				options.Label("SPL.Settings.SeedFactorModifier".Translate("100%", "20%", "500%") + seedFactorModifier.ToStringPercent(), -1f, "SPL.Settings.SeedFactorModifier.Desc".Translate("1"));
+				seedFactorModifier = options.Slider(seedFactorModifier, 0.2f, 5f);
+
+				options.CheckboxLabeled("SPL.Settings.NoUselessSeeds".Translate(), ref noUselessSeeds, "SPL.Settings.NoUselessSeeds.Desc".Translate());
+				options.CheckboxLabeled("SPL.Settings.ClearSnow".Translate(), ref clearSnow, "SPL.Settings.ClearSnow.Desc".Translate());
+
+				options.CheckboxLabeled("SPL.Settings.EdibleSeeds".Translate(), ref edibleSeeds, "SPL.Settings.EdibleSeeds.Desc".Translate());
 				options.End();
-			Widgets.EndScrollView();
-			
+			}
+			else if (selectedTab == Tab.seedless)
+			{
+				Widgets.DrawMenuSection(contentRect);
+				// Draw restart label above the scroll view
+				Rect restartRect = new Rect(contentRect.x, contentRect.y, contentRect.width, 28f);
+				Widgets.Label(restartRect, "SPL.RequiresRestart".Translate());
+				float scrollY = contentRect.y + restartRect.height + 4f;
+				float scrollHeight = contentRect.height - restartRect.height - 4f;
+				Rect scrollRect = new Rect(contentRect.x, scrollY, contentRect.width, scrollHeight);
+				Rect innerRect = new Rect(0f, 0f, contentRect.width - 16f, (OptionsDrawUtility.lineNumber + 1) * 22f); // +1 for extra space
+				Widgets.BeginScrollView(scrollRect, ref scrollPos, innerRect, true);
+				Listing_Standard options = new Listing_Standard();
+				options.Begin(innerRect);
+				options.DrawList(innerRect);
+				options.End();
+				Widgets.EndScrollView();
+			}
+			//else if (selectedTab == Tab.labels)
+			//{
+			//    Listing_Standard options = new Listing_Standard();
+			//    options.Begin(contentRect);
+			//    options.Label("SPL.Settings.LabelsDesc".Translate());
+			//    options.End();
+			//}
 			GUI.EndGroup();
 		}
 
@@ -126,8 +123,8 @@ namespace SeedsPleaseLite
 		public static bool noUselessSeeds = true, clearSnow, edibleSeeds = true;
 		public static HashSet<string> seedlessInversions;
 		public static HashSet<ushort> seedlessCache;
-		public static Tab selectedTab = Tab.seedless;
-		public enum Tab { seedless, labels };
+		public static Tab selectedTab = Tab.main;
+		public enum Tab { main, seedless, labels };
 		public static Vector2 scrollPos;
 	}
 }
